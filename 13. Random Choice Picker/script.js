ff09499116bcc6dd2a611000b3e43743c9665a08
Oliver Thorn <thornow11@gmail.com>
2022-02-28 14:56:02 +0000

    13. Complete

--- 13. Random Choice Picker/script.js
+++ 13. Random Choice Picker/script.js
@@ -4,7 +4,14 @@ const textNode = document.getElementById('textarea');
 textarea.focus();
 
 textarea.addEventListener('keyup', (e) => {
-    createTags(e.target.value)
+    createTags(e.target.value);
+
+    if (e.key == 'Enter') {
+        setTimeout(() => {
+            e.target.value = '';
+        }, 10)
+        randomSelect();
+    }
 })
 
 function createTags(input) {
@@ -20,4 +27,39 @@ function createTags(input) {
         tagNode.innerText = tag;
         tagsNode.appendChild(tagNode);
     })
+}
+
+function randomSelect() {
+    const times = 30;
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+        highlightTag(randomTag);
+        setTimeout(() => {
+            unhighlightTag(randomTag);
+        }, 100);
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+            highlightTag(randomTag);
+        }, 100)
+
+    }, times * 100)
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag');
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+
+function unhighlightTag(tag) {
+    tag.classList.remove('highlight')
 }
